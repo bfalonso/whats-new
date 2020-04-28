119136b7c26dc9b884be3f5bb306c26b9b88e9e0
Brais <bfalonso@esei.uvigo.es>
2020-04-28 15:45:16 +0200

    Adds main view
    No data yet

--- handlers/main.py
+++ handlers/main.py
@@ -15,10 +15,26 @@
 # limitations under the License.
 #
 import webapp2
+from webapp2_extras import jinja2
+from webapp2_extras.users import users
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        user = users.get_current_user()
+
+        if user:
+            url_user = users.create_logout_url("/")
+        else:
+            url_user = users.create_login_url("/")
+
+        jinja = jinja2.get_jinja2(app=self.app)
+
+        valores = {
+            "user": user,
+            "url_user": url_user
+        }
+
+        self.response.write(jinja.render_template("index.html", **valores))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
